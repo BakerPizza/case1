60f058625bd6d8f1f9f14a81d44c29453a38332b
Sklyaraa <100130873+Sklyaraa@users.noreply.github.com>
2022-02-21 14:08:46 +0300

    Update showposts.php

--- views/showposts.php
+++ views/showposts.php
@@ -2,7 +2,7 @@
 
 $db = new Database();
 
-$array= $db->query("SELECT * FROM `blog`");  //выбрать всё из таблицы blog
+$array= $db->query("SELECT * FROM `blog`");  //выбрать все записи из таблицы blog
 
 foreach($array as $row){
     echo "<h4>".$row['title_post']."</h4><br><p>".$row['description_post']."<p><br>";  //вывести title_post,description_post	
