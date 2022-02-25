9291e88ebabd2b0a0944f6eaa0af2ba476424242
Sarnovskiy Daniil <99619436+BakerPizza@users.noreply.github.com>
2022-02-25 13:52:22 +0300

    Update avatar_change.php

--- cod/avatar_change.php
+++ cod/avatar_change.php
@@ -4,7 +4,7 @@ require_once('connections.php');
 $id_user = $_SESSION['id_user'];
 if(isset($_POST['change'])){      
 $img_type = substr($_FILES['avatar_upload_change']['type'],0,5);
-if(!empty($_FILES['avatar_upload_change']['tmp_name']) and $img_type=='image'){     
+if(!empty($_FILES['avatar_upload_change']['tmp_name']) and $img_type=='image'){
     $img = addslashes(file_get_contents($_FILES['avatar_upload_change']['tmp_name']));
     $post = mysqli_query($connect, "UPDATE `users` SET `avatar` = '$img' WHERE `id_user` = '$id_user'");
     header('Location: ../personal.php');
