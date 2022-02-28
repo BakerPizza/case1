3af985ccca5c666a64a8aee9074bb78a6acbb7d2
Sarnovskiy Daniil <99619436+BakerPizza@users.noreply.github.com>
2022-02-28 11:06:33 +0300

    Update post_add.php

--- cod/post_add.php
+++ cod/post_add.php
@@ -5,10 +5,10 @@ $title = $_POST['title'];
 $text = $_POST['text'];
 $id_user = $_SESSION['id_user'];
 $img_type = substr($_FILES['img']['type'],0,5);
-if(!empty($_FILES['img']['tmp_name']) and $img_type=='image'){     
+if(!empty($_FILES['img']['tmp_name']) and $img_type=='image'){     //проверка на фото 
     $img = addslashes(file_get_contents($_FILES['img']['tmp_name']));
     $post = mysqli_query($connect, "INSERT INTO `post` (`id_post`, `id_user`, `title`, `text`, `img`, `time`) VALUES (NULL, '$id_user', '$title', '$text', '$img', NULL)");
-    header('Location: ../index.php');
+    header('Location: ../index.php');//ввод данных в бд
     echo "done";
 }
 else{
