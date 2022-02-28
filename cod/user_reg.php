282735778bbb38260b1934d8fa1e87d392d52061
Sarnovskiy Daniil <99619436+BakerPizza@users.noreply.github.com>
2022-02-28 11:05:36 +0300

    Update user_reg.php

--- cod/user_reg.php
+++ cod/user_reg.php
@@ -4,13 +4,13 @@ $log = $_POST['log'];
 $par = $_POST['par'];
 $email = $_POST['email'];
 $nick = $_POST['nick'];
-$resul = mysqli_query($connect, "SELECT password FROM users where login='$log'");
+$resul = mysqli_query($connect, "SELECT password FROM users where login='$log'"); //проверка пароль
 if ($resul)
     {
     $sum_row_users = mysqli_num_rows($resul);	
     if ($sum_row_users==0)
         {	
-        $addgolos = mysqli_query($connect, "INSERT INTO `users` (`id_user`, `nickname`, `login`, `password`, `email`, `avatar`, `id_rol`) VALUES (NULL, '$nick', '$log', '$par', '$email', '1', '1')");
+        $addgolos = mysqli_query($connect, "INSERT INTO `users` (`id_user`, `nickname`, `login`, `password`, `email`, `avatar`, `id_rol`) VALUES (NULL, '$nick', '$log', '$par', '$email', '1', '1')");//ввод данных в бд
         $prov = mysqli_query($connect, "SELECT `login` FROM `users`");
         echo "done";   
         }
@@ -19,3 +19,15 @@ if ($resul)
    
     }   
 
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
